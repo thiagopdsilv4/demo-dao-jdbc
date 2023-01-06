92eff1486cfdd1ceb2f720b7b097b33ca9aae514
Thiago Pinheiro <thiagopdsilva1@gmail.com>
2023-01-06 15:48:17 -0300

    FindById

--- demo-dao-jdbc/src/application/Program.java
+++ demo-dao-jdbc/src/application/Program.java
@@ -3,6 +3,7 @@ package application;
 import java.util.Date;
 
 import model.dao.DaoFactory;
+import model.dao.DaoFactoryy;
 import model.dao.SellerDao;
 import model.entities.Department;
 import model.entities.Seller;
@@ -11,12 +12,14 @@ public class Program {
 
 	public static void main(String[] args) {
 
-		Department obj = new Department(1, "Books");
+		//Department obj = new Department(1, "Books");
 		
-		Seller seller = new Seller(21, "Bob", "Bob@bob.com", new Date(), 3000.00, obj);
-		SellerDao sellerDao = DaoFactory.createSellerDao();//deessa forma o programa não conhece a implementação, somente a interface.
+		//SellerDao sellerDao = DaoFactory.createSellerDao();//deessa forma o programa não conhece a implementação, somente a interface.
+		//Seller sellerD = sellerDao.findById(3);
 		
-		System.out.println(obj);
+		SellerDao sellerDao = DaoFactoryy.createSellerDao();
+		
+		Seller seller = sellerDao.findById(3);
 		System.out.println(seller);
 	}
 
