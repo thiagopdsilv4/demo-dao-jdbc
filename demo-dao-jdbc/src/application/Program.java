78d29eae36706f07dae8e4a328f6d8fc698a7251
Thiago Pinheiro <thiagopdsilva1@gmail.com>
2023-01-06 15:52:23 -0300

    Reusando instanciacao

--- demo-dao-jdbc/src/application/Program.java
+++ demo-dao-jdbc/src/application/Program.java
@@ -19,6 +19,7 @@ public class Program {
 		
 		SellerDao sellerDao = DaoFactoryy.createSellerDao();
 		
+		System.out.println("=== TEST 1: seller findById ===");
 		Seller seller = sellerDao.findById(3);
 		System.out.println(seller);
 	}
--- demo-dao-jdbc/src/model/dao/impl/SellerDaoJDBC.java
+++ demo-dao-jdbc/src/model/dao/impl/SellerDaoJDBC.java
@@ -55,13 +55,7 @@ public class SellerDaoJDBC implements SellerDao{
 			
 			if(rs.next()) {//implementação para retornar um vendedor por id
 				Department dep = instantiateDepartment(rs);
-				Seller obj = new Seller();
-				obj.setId(rs.getInt("Id"));
-				obj.setName(rs.getString("Name"));
-				obj.setEmail(rs.getString("Email"));
-				obj.setBaseSalary(rs.getDouble("BaseSalary"));
-				obj.setBirthDate(rs.getDate("BirthDate"));
-				obj.setDepartment(dep);
+				Seller obj = instantiateSeller(rs, dep);
 				return obj;
 			}
 			return null;
@@ -73,6 +67,17 @@ public class SellerDaoJDBC implements SellerDao{
 			DB.closeStatement(st);
 			DB.closeResultSet(rs);
 		}
+	}
+
+	private Seller instantiateSeller(ResultSet rs, Department dep) throws SQLException {
+		Seller obj = new Seller();
+		obj.setId(rs.getInt("Id"));
+		obj.setName(rs.getString("Name"));
+		obj.setEmail(rs.getString("Email"));
+		obj.setBaseSalary(rs.getDouble("BaseSalary"));
+		obj.setBirthDate(rs.getDate("BirthDate"));
+		obj.setDepartment(dep);
+		return obj;
 	}
 
 	private Department instantiateDepartment(ResultSet rs) throws SQLException {
